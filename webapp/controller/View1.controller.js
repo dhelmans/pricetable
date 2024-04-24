c346bc3f150d8bc0a381beb222c0c9bf9e98ed32
Matheus Delmas <mdvfdo@gmail.com>
2024-04-24 18:12:20 +0000

    Add ProductID Filter on screen and option to filter both, Product Name and ID, alone or together

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,7 +1,7 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
+    "sap/ui/model/FilterOperator"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
@@ -14,21 +14,25 @@ sap.ui.define([
 
             },
 
-            onGetProduct: function(oEvent){
+            onGetProductBy: function () {
 
                 const aFilter = [];
+                const sName = this.byId("filterName").getValue();
+                const iID = this.byId("filterID").getValue();
 
-                const sSearch = oEvent.getParameter("newValue");
-                
-                if (sSearch) {
-                   aFilter.push(new Filter("ProductName", FilterOperator.Contains, sSearch))
+                if (sName) {
+                    aFilter.push(new Filter("ProductName", FilterOperator.Contains, sName));
+                }
+
+                if (iID) {
+                    aFilter.push(new Filter("ProductID", FilterOperator.EQ, iID));
                 }
 
                 const oItems = this.byId("priceTable").getBinding("items");
                 oItems.filter(aFilter)
             },
 
-            onConvTextToNum: function(sString){
+            onConvTextToNum: function (sString) {
                 if (sString) {
                     return parseInt(sString);
                 }
